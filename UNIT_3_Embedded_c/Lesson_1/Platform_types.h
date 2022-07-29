b341d5218004c5a570360c4c95add429ce2b6bf7
Abdalla7Ahmed <abdallahahmed17120@gmail.com>
2022-07-29 16:44:11 +0200

    Embedded c L_1

---
+++ UNIT_3_Embedded_c/Lesson_1/Platform_types.h
@@ -0,0 +1,36 @@
+
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+
+typedef unsigned char                 uint8_t;
+typedef signed char                   int8_t;
+
+typedef unsigned short                uint16_t;
+typedef signed short                  int16_t;
+
+typedef unsigned int                  uint32_t;
+typedef signed int                    int32_t;
+
+typedef float                         float32_t;
+typedef double                        float64_t;
+typedef long double                   float48_t;
+
+/************* volatile *************/
+
+typedef volatile unsigned char        vuint8_t;
+typedef volatile signed char          vint8_t;
+
+typedef volatile unsigned short       vuint16_t;
+typedef volatile signed short         vint16_t;
+
+typedef volatile unsigned int         vuint32_t;
+typedef volatile signed int           vint32_t;
+
+typedef volatile float                vfloat32_t;
+typedef volatile double               vfloat64_t;
+typedef volatile long double          vfloat48_t;
+
+
+#endif
+
+
