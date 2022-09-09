c50b1d9644140e9129f1e556f803d84be59ec7f2
Abdalla7Ahmed <abdallahahmed17120@gmail.com>
2022-09-09 04:55:19 +0200

    Lesson_3

--- Unit_6 Micro-controller Arch/Lesson_3_MCU_Clock/Lab_3/Lab_3.c
+++ Unit_6 Micro-controller Arch/Lesson_3_MCU_Clock/Lab_3/Lab_3.c
@@ -25,47 +25,6 @@
 #define RCC_CFGR     *(volatile uint32_t *)(RCC_BASE + 0x04)
 #define RCC_CR       *(volatile uint32_t *)(RCC_BASE + 0x00)
 
-typedef union {
-	uint32_t all_port;
-	struct {
-		uint32_t Pin0:1;
-		uint32_t Pin1:1;
-		uint32_t Pin2:1;
-		uint32_t Pin3:1;
-		uint32_t Pin4:1;
-		uint32_t Pin5:1;
-		uint32_t Pin6:1;
-		uint32_t Pin7:1;
-		uint32_t Pin8:1;
-		uint32_t Pin9:1;
-		uint32_t Pin10:1;
-		uint32_t Pin11:1;
-		uint32_t Pin12:1;
-		uint32_t Pin13:1;
-		uint32_t Pin14:1;
-		uint32_t Pin15:1;
-		uint32_t Pin16:1;
-		uint32_t Pin17:1;
-		uint32_t Pin18:1;
-		uint32_t Pin19:1;
-		uint32_t Pin20:1;
-		uint32_t Pin21:1;
-		uint32_t Pin22:1;
-		uint32_t Pin23:1;
-		uint32_t Pin24:1;
-		uint32_t Pin25:1;
-		uint32_t Pin26:1;
-		uint32_t Pin27:1;
-		uint32_t Pin28:1;
-		uint32_t Pin29:1;
-		uint32_t Pin30:1;
-		uint32_t Pin31:1;
-	}PIN;
-
-}ODR_t;
-
-volatile ODR_t *PODR=(volatile ODR_t *)(GPIOA_BASE+0x0c);
-
 
 int main(void)
 {
@@ -92,12 +51,12 @@ int main(void)
 	RCC_APB2ENR |= (1<<2);
 	GPIOA_CRH &= 0xFF0FFFFF;
 	GPIOA_CRH |= 0x00200000;
-
+	
 	while(1)
 	{
-		PODR->PIN.Pin13=1;
+		GPIOA_ODR |=(1<<13);
 		for(int i=0; i<1000;i++);
-		PODR->PIN.Pin13=0;
+		GPIOA_ODR &=~(1<<13);
 		for(int i=0;i<1000;i++);
 	}
 
